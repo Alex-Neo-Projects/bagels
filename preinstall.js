929ffa13efd3efbdb256754a188932f5732629fe
Neo <choneo.ch@gmail.com>
2023-01-18 20:05:48 -0800

    add preinstall back

---
+++ preinstall.js
@@ -0,0 +1,34 @@
+import { spawn } from 'child_process'
+
+async function main() {
+  try {
+    spawn(
+      'curl',
+      [
+        '-L',
+        'https://foundry.paradigm.xyz',
+        '|',
+        'bash',
+        '&&',
+        'foundryup',
+        '&&',
+        'curl',
+        '-fsSL',
+        'https://bun.sh/install',
+        '|',
+        'bash'
+      ],
+      {
+        shell: true,
+        stdio: 'inherit'
+      },
+    )
+  } catch (e) {
+    console.error('Unable to download required dependencies: [foundry, bun]')
+  }
+}
+
+main().catch((err) => {
+  console.error(err.message)
+  process.exit(1)
+})
