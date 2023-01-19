eba73e44f88c46ea655bc91458d57773d3cda915
Neo <choneo.ch@gmail.com>
2023-01-18 22:45:13 -0800

    remove sum sutff

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, BrowserRouter } from 'react-router-dom'
-import Home from './components/Home'
-import Contracts from './components/Contracts'
-import NotFound from './components/NotFound'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+} from 'react-router-dom'
+import Home from './routes/Home'
+import Contracts from './routes/Contracts'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-    errorElement: <NotFound />,
   },
   {
     path: '/contracts/:contractId',
     element: <Contracts />,
-    errorElement: <NotFound />,
   },
 ])
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <RouterProvider router={router} />
-  </BrowserRouter>,
+  <RouterProvider router={router} />,
 )
