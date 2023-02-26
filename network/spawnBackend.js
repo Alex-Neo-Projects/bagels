daa0b2ffb0810645fa9e0499aa1af455d25fb006
Jeremy Osborne <jeremy.osborne@du.edu>
2023-02-26 14:34:55 -0700

    updated localhost port value in header

--- network/spawnBackend.js
+++ network/spawnBackend.js
@@ -1,67 +1,72 @@
 #!/usr/bin/env node
+
 import { spawn } from "child_process";
 import { parentPort, isMainThread, workerData } from "worker_threads";
 import { getFilepath, getPathDirname } from "../utils.js";
 
 async function main() {
-  const serverDir = getFilepath([getPathDirname(), "server"]);
+    const serverDir = getFilepath([getPathDirname(), "server"]);
 
-  const serverToLookIn = process.cwd();
+    const serverToLookIn = process.cwd();
 
-  if (!serverToLookIn) {
-    console.log("error starting server, plz restart bagels!");
-    return;
-  }
-
-  let networkUrl = ""
-
-  if (workerData && workerData["network"]) {
-    if (workerData["network"] === "mainnet") {
-      console.log("Forking Mainnet...");
-      networkUrl =
-        "https://eth-mainnet.g.alchemy.com/v2/YKOGR_zFYv0ouTsGab24VKwOm5w7k6QZ";
-    } else if (workerData["network"] === "polygon") {
-      console.log("Forking Polygon...");
-      networkUrl =
-        "https://polygon-mainnet.g.alchemy.com/v2/fZritfEhq_gYVJ8cV97PHgwVUm9V_Dnx";
-    } else {
-      console.log(
-        `Forking network: ${workerData["network"]} is not an option.`
-      );
-      console.log(
-        `Bagels only supports forking mainnet or polygon with: bagels --fork mainnet and bagels --fork polygon\n`
-      );
+    if (!serverToLookIn) {
+        console.log("error starting server, plz restart bagels!");
+        return;
     }
-  }
 
-  const nodeProcess = spawn("node", ["startServer.js", serverToLookIn, networkUrl], {
-    cwd: serverDir,
-  });
+    let networkUrl = ""
 
-  nodeProcess.stdout.on("data", (data) => {
-    if (data.toString().includes("server started and listening for requests")) {
-      parentPort.postMessage("started");
-    } else {
-      console.log(data.toString());
+    if (workerData && workerData["network"]) {
+        if (workerData["network"] === "mainnet") {
+            console.log("Forking Mainnet...");
+            networkUrl =
+                "https://eth-mainnet.g.alchemy.com/v2/YKOGR_zFYv0ouTsGab24VKwOm5w7k6QZ";
+        } else if (workerData["network"] === "polygon") {
+            console.log("Forking Polygon...");
+            networkUrl =
+                "https://polygon-mainnet.g.alchemy.com/v2/fZritfEhq_gYVJ8cV97PHgwVUm9V_Dnx";
+        } else {
+            console.log(
+                `Forking network: ${workerData["network"]} is not an option.`
+            );
+            console.log(
+                `Bagels only supports forking mainnet or polygon with: bagels --fork mainnet and bagels --fork polygon\n`
+            );
+        }
     }
-  });
-
-  nodeProcess.stderr.on("data", (data) => {
-    if (data.toString().includes("EADDRINUSE: address already in use")) {
-      console.error("Error starting the server \n");
-      console.log("There is already a process on port 9090\n");
-      console.log("to kill the process, run: `lsof -i tcp:9090`\n");
-      console.log(
-        "then run: `kill {port}` with the port printed from the above command\n"
-      );
-    } else {
-      console.error("Error starting the server: ", data.toString());
+    let ganachePort = "";
+    if (workerData && workerData["ganachePort"]) {
+        ganachePort = workerData["ganachePort"];
     }
-  });
 
-  if (!isMainThread) {
-    parentPort.postMessage(nodeProcess.pid);
-  }
+    const nodeProcess = spawn("node", ["startServer.js", serverToLookIn, networkUrl, ganachePort], {
+        cwd: serverDir,
+    });
+
+    nodeProcess.stdout.on("data", (data) => {
+        if (data.toString().includes("server started and listening for requests")) {
+            parentPort.postMessage("started");
+        } else {
+            console.log(data.toString());
+        }
+    });
+
+    nodeProcess.stderr.on("data", (data) => {
+        if (data.toString().includes("EADDRINUSE: address already in use")) {
+            console.error("Error starting the server \n");
+            console.log("There is already a process on port 9090\n");
+            console.log("to kill the process, run: `lsof -i tcp:9090`\n");
+            console.log(
+                "then run: `kill {port}` with the port printed from the above command\n"
+            );
+        } else {
+            console.error("Error starting the server: ", data.toString());
+        }
+    });
+
+    if (!isMainThread) {
+        parentPort.postMessage(nodeProcess.pid);
+    }
 }
 
 main();
--- network/startNetwork.js
+++ network/startNetwork.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import { Worker } from "worker_threads";
 import process, { kill } from "process";
 import open from "open";
@@ -11,150 +12,158 @@ import { execSync } from "child_process";
 const PLATFORM = platform;
 
 const client = new PostHog("phc_XmppwnWycFgtoeRJR93d1QaiYtZ4CPSJs4Dts5uTRm4", {
-  host: "https://app.posthog.com",
+    host: "https://app.posthog.com",
 });
 
 let children = [];
 
 async function workerPromise(script, data) {
-  return await new Promise((resolve, reject) => {
-    let w = new Worker(script, { workerData: data });
-    let pid;
+    return await new Promise((resolve, reject) => {
+        let w = new Worker(script, { workerData: data });
+        let pid;
 
-    w.on("message", (message) => {
-      // This means that the worker is done *starting*
-      // Necessary to avoid race condition where the backend is slower to start than the frontend
-      // We resolve the PID anyway though, because it's used to kill the process later
-      if (message === "started") {
-        resolve(pid);
-      } else {
-        pid = message;
-      }
+        w.on("message", (message) => {
+            // This means that the worker is done *starting*
+            // Necessary to avoid race condition where the backend is slower to start than the frontend
+            // We resolve the PID anyway though, because it's used to kill the process later
+            if (message === "started") {
+                resolve(pid);
+            } else {
+                pid = message;
+            }
+        });
+        w.on("error", (err) => reject(err));
+        w.on("exit", (exitCode) => {
+            reject(`worker finished with exit code ${exitCode}`);
+        });
     });
-    w.on("error", (err) => reject(err));
-    w.on("exit", (exitCode) => {
-      reject(`worker finished with exit code ${exitCode}`);
-    });
-  });
 }
 
 function checkIfRunning() {
-  // Check either anvil or backend or frontend are running
-  switch (PLATFORM) {
-    case "linux":
-    case "darwin":
-      // backend
-      try {
-        execSync("lsof -ti tcp:9090 | xargs -r kill");
-      } catch (e) {
-        return false
-      }
+    // Check either anvil or backend or frontend are running
+    switch (PLATFORM) {
+        case "linux":
+        case "darwin":
+            // backend
+            try {
+                execSync("lsof -ti tcp:9090 | xargs -r kill");
+            } catch (e) {
+                return false
+            }
 
-      // frontend
-      try {
-        execSync("lsof -ti tcp:1274 | xargs -r kill");
-      } catch (e) {
-        return false
-      }
+            // frontend
+            try {
+                execSync("lsof -ti tcp:1274 | xargs -r kill");
+            } catch (e) {
+                return false
+            }
 
-      return true
-    case "win32":
-      // backend
-      try {
-        execSync("(Get-NetTCPConnection -LocalPort 9090) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }", { shell: "powershell.exe", stdio: "ignore" });
-      } catch (e) {
-        return false
-      }
+            return true
+        case "win32":
+            // backend
+            try {
+                execSync("(Get-NetTCPConnection -LocalPort 9090) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }", { shell: "powershell.exe", stdio: "ignore" });
+            } catch (e) {
+                return false
+            }
 
-      // frontend
-      try {
-        execSync("(Get-NetTCPConnection -LocalPort 1274) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }", { shell: "powershell.exe", stdio: "ignore"});
-      } catch (e) {
-        return false
-      }
+            // frontend
+            try {
+                execSync("(Get-NetTCPConnection -LocalPort 1274) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }", { shell: "powershell.exe", stdio: "ignore" });
+            } catch (e) {
+                return false
+            }
 
-      return true
-    default:
-      console.log("Unable to check platform, moving on!");
-      return true
-  }
+            return true
+        default:
+            console.log("Unable to check platform, moving on!");
+            return true
+    }
 }
 
 async function main() {
-  if(!checkIfRunning()) {
-    console.log("Unable to start bagels, try running these commands to reset the backend and frontend: \n")
+    if (!checkIfRunning()) {
+        console.log("Unable to start bagels, try running these commands to reset the backend and frontend: \n")
 
-    if(PLATFORM === 'win32') {
-      console.log("(Get-NetTCPConnection -LocalPort 9090) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }")
-      console.log("(Get-NetTCPConnection -LocalPort 1274) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }\n")
-    }else if(PLATFORM === 'linux' || PLATFORM === 'darwin') {
-      console.log("lsof -ti tcp:9090 | xargs kill")
-      console.log("lsof -ti tcp:1274 | xargs kill \n")
-    }else {
-      console.log("🥯 Add an issue to bagels if you see this!")
+        if (PLATFORM === 'win32') {
+            console.log("(Get-NetTCPConnection -LocalPort 9090) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }")
+            console.log("(Get-NetTCPConnection -LocalPort 1274) | Select-Object -ExpandProperty OwningProcess | ForEach-Object { Stop-Process $_ }\n")
+        } else if (PLATFORM === 'linux' || PLATFORM === 'darwin') {
+            console.log("lsof -ti tcp:9090 | xargs kill")
+            console.log("lsof -ti tcp:1274 | xargs kill \n")
+        } else {
+            console.log("🥯 Add an issue to bagels if you see this!")
+        }
+
+        process.exit(1)
     }
 
-    process.exit(1)
-  }
+    try {
+        let filePaths = {
+            backendPath: getFilepath([
+                getPathDirname(),
+                "network",
+                "spawnBackend.js",
+            ]),
+            frontendPath: getFilepath([getPathDirname(), "network", "spawnUI.js"]),
+        };
 
-  try {
-    let filePaths = {
-      backendPath: getFilepath([
-        getPathDirname(),
-        "network",
-        "spawnBackend.js",
-      ]),
-      frontendPath: getFilepath([getPathDirname(), "network", "spawnUI.js"]),
-    };
+        let forkNetwork;
 
-    let forkNetwork;
+        // Only args is forking
+        if (process.argv.indexOf("--fork") >= 0) {
+            // Just get whatever is after --fork
+            forkNetwork = process.argv[process.argv.indexOf("--fork") + 1];
+        }
 
-    // Only args is forking
-    if (process.argv.indexOf("--fork") >= 0) {
-      // Just get whatever is after --fork
-      forkNetwork = process.argv[process.argv.indexOf("--fork") + 1];
+        let ganachePort;
+
+        if (process.argv.indexOf("--ganache-port") >= 0) {
+            ganachePort = process.argv[process.argv.indexOf("--ganache-port") + 1];
+            console.log("G" + ganachePort);
+        }
+
+        const backendWorker = await workerPromise(filePaths.backendPath, {
+            network: forkNetwork,
+            ganachePort: ganachePort,
+        });
+        if (!backendWorker) {
+            console.log("Unable to start the backend");
+            process.exit(1);
+        }
+
+        children.push(backendWorker);
+
+        const uiWorker = await workerPromise(filePaths.frontendPath);
+        if (!uiWorker) {
+            console.log("Unable to start frontend");
+            process.exit(1);
+        }
+        children.push(uiWorker);
+
+        // Capture bagels starts
+        client.capture({
+            distinctId: os.userInfo().username,
+            event: "bagels-started",
+        });
+
+        // Start Local Host
+        open("http://localhost:9091/");
+    } catch (e) {
+        console.error(e.message);
+        process.exit(1);
     }
 
-    const backendWorker = await workerPromise(filePaths.backendPath, {
-      network: forkNetwork
+    process.on("SIGINT", () => {
+        console.log("\n🥯 Closing Bagels!");
+        if (children.length >= 1) {
+            children.forEach((child) => {
+                kill(child, "SIGTERM");
+            });
+            children = [];
+        }
+        process.exit(1);
     });
-    if (!backendWorker) {
-      console.log("Unable to start the backend");
-      process.exit(1);
-    }
-
-    children.push(backendWorker);
-
-    const uiWorker = await workerPromise(filePaths.frontendPath);
-    if (!uiWorker) {
-      console.log("Unable to start frontend");
-      process.exit(1);
-    }
-    children.push(uiWorker);
-
-    // Capture bagels starts
-    client.capture({
-      distinctId: os.userInfo().username,
-      event: "bagels-started",
-    });
-
-    // Start Local Host
-    open("http://localhost:9091/");
-  } catch (e) {
-    console.error(e.message);
-    process.exit(1);
-  }
-
-  process.on("SIGINT", () => {
-    console.log("\n🥯 Closing Bagels!");
-    if (children.length >= 1) {
-      children.forEach((child) => {
-        kill(child, "SIGTERM");
-      });
-      children = [];
-    }
-    process.exit(1);
-  });
 }
 
 main();
